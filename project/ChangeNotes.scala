444788e23b369d060ed2c5f70958d5d3b8d5d5cf
Rafal Piotrowski <rpiotrowski@iteratorshq.com>
2021-12-01 12:40:36 +0100

    Support for intellij 2021.3.*

--- project/ChangeNotes.scala
+++ project/ChangeNotes.scala
@@ -1,9 +1,6 @@
 object ChangeNotes {
   val value =
     """<![CDATA[
-    Support for upcoming versions of IntelliJ (2021.2.*).
-    <ul>
-      <li>support for IntelliJ IDEA 2021.2.*</li>
-    </ul>
+    Support for IntelliJ IDEA 2021.3.*.
   ]]>"""
 }
