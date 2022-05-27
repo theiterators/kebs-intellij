161430c07f7fddbcb4b331edd42705e569436d6d
Rafal Piotrowski <rpiotrowski@iteratorshq.com>
2022-05-27 22:05:59 +0200

    Set version to 0.0.9 and fill change notes

--- project/ChangeNotes.scala
+++ project/ChangeNotes.scala
@@ -1,6 +1,10 @@
 object ChangeNotes {
   val value =
     """<![CDATA[
-    Support for IntelliJ IDEA 2022.1.*.
+    Bugfix and support for upcoming version of IntelliJ.
+    <ul>
+      <li>(bugfix #11) "Generate @tagged object" does not work for String in 2022.1</li>
+      <li>support for IntelliJ IDEA 2022.2.*</li>
+    </ul>
   ]]>"""
 }
