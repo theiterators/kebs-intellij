626194065bcc46e613c6d649128213344ec9bae3
Rafal Piotrowski <rpiotrow@users.noreply.github.com>
2023-05-14 17:50:05 +0200

    Revert "Support for intellij 2022.3.*"
    This reverts commit 073b284182338ee5859b2ce1dcd3fa1eedd6d0b9.

--- project/ChangeNotes.scala
+++ project/ChangeNotes.scala
@@ -3,7 +3,8 @@ object ChangeNotes {
     """<![CDATA[
     Bugfix and support for upcoming version of IntelliJ.
     <ul>
-      <li>support for IntelliJ IDEA 2022.3.*</li>
+      <li>(bugfix #11) "Generate @tagged object" does not work for String in 2022.1</li>
+      <li>support for IntelliJ IDEA 2022.2.*</li>
     </ul>
   ]]>"""
 }
